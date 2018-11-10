4728d37d030b2159f4fa6ed5538784590c5456fb
Minerva Gao <minervagao2002@gmail.com>
2018-11-10 19:34:47 +0000

    your message

--- javascript.js
+++ javascript.js
@@ -1,17 +1,17 @@
 //javascript.js
 
 var slideIndex = 0;
-
+showSlides();
 function showSlides() {
     
     var slides = document.getElementsByClassName("myimages");
     //var slides =[img1, img2, img3]
-    for (var i = 0; i< slides.length; i++){
+    for (var i = 0; i< slides.length; i++) {
         slides[i].style.display="none";
     }
     slideIndex++;
     if (slideIndex >= slides.length){
-        slide=0;
+        slideIndex=0;
     }
     slides[slideIndex].style.display = "block";
     setTimeout(showSlides, 4000);
@@ -20,4 +20,5 @@ function showSlides() {
 //1st loop: i = 0, slides[0] = img1
 //2nd loop: i = 1, slides[1] = img2
 //3rd loop: i = 2, slides[2] = img3
+//4th loop: i =, slides[] = img
     
